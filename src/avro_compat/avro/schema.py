5866635fbc179c213313a345a649174e2e4ac59f
Steve Stagg <stestagg@gmail.com>
2023-08-17 22:33:52 +0000

    Make more tests pass

--- src/avro_compat/avro/schema.py
+++ src/avro_compat/avro/schema.py
@@ -5,9 +5,22 @@ import json
 import avro_compat.avro.constants
 
 
+
 class AvroTypeMeta(type):
     def __instancecheck__(cls, inst) -> bool:
-        return isinstance(inst, cavro.Schema) and isinstance(inst.type, cls.TYPE)
+        if not isinstance(inst, (cavro.Schema, cavro.LogicalType)):
+            return False
+        if not isinstance(inst.type, cls.TYPE):
+            return False
+        if logical_type := getattr(cls, 'LOGICAL_TYPE', None):
+            if not isinstance(inst.type.logical_type, logical_type):
+                return False
+        return True
+
+
+class DecimalLogicalSchema(metaclass=AvroTypeMeta):
+    TYPE = (cavro.BytesType, cavro.FixedType)
+    LOGICAL_TYPE = cavro.DecimalType
 
 
 class ArraySchema(metaclass=AvroTypeMeta):
@@ -76,11 +89,25 @@ class Schema(cavro.Schema):
     def __getattr__(self, name):
         return getattr(self.type, name)
 
+    @property
+    def logicalType(self):
+        return self.type.logical_type.logical_name
+
+    def get_prop(self, prop):
+        if self.type.logical_type is not None:
+            try:
+                return getattr(self.type.logical_type, prop)
+            except AttributeError:
+                pass
+        return getattr(self, prop)
+        
+
 
 def parse(json_string: str, validate_enum_symbols: bool = True, validate_names: bool = True) -> Schema:
     options = cavro.Options(
         enum_symbols_must_be_unique=validate_enum_symbols,
         enforce_enum_symbol_name_rules=validate_enum_symbols,
         types_str_to_schema=True,
+        fingerprint_returns_digest=True,
     )
     return Schema(json_string, options=options)
